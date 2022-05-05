67c06f1a326b44d732ec45ffe3c5f463b5325c52
wileyj <2847772+wileyj@users.noreply.github.com>
2022-05-05 16:17:50 -0700

    update for source repo changes
    - update version to match stacks-blockchain
    - move 03-bns.sj -> 03-env.sh since we're enabling a few extra vars now
    - update unit file to reflect repo changes to non-positional args

--- scripts/03-env.sh
+++ scripts/03-env.sh
@@ -3,6 +3,13 @@
 echo "=== Enable stacks-blockchain-api BNS data ==="
 sed -i -e 's|# BNS_IMPORT_DIR|BNS_IMPORT_DIR|' /opt/stacks-blockchain-docker/.env
 
+echo "=== Enable stacks-blockchain-api fungible metadata ==="
+sed -i -e 's|# STACKS_API_ENABLE_FT_METADATA|STACKS_API_ENABLE_FT_METADATA|' /opt/stacks-blockchain-docker/.env
+
+echo "=== Enable stacks-blockchain-api non-fungible metadata ==="
+sed -i -e 's|# STACKS_API_ENABLE_NFT_METADATA|STACKS_API_ENABLE_NFT_METADATA|' /opt/stacks-blockchain-docker/.env
+
+
 echo "=== Downloading BNS Data ==="
 export BNS_IMPORT_DIR="/opt/stacks-blockchain-docker/persistent-data/bns-data" && \
     mkdir -p ${BNS_IMPORT_DIR} && \
